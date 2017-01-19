8b692a4de64143f9b513fa6aa506c0f6938dc942
jadrdc <jadrdc@gmail.com>
2017-01-19 00:30:59 -0400

    Segundo Commit de Tarea 1

--- Tarea-1/routes/main.js
+++ Tarea-1/routes/main.js
@@ -1,27 +1,47 @@
-const  router=require('express').Router();
+const router = require('express').Router();
 const UserPayment = require('../models/employeepayment');
-const fileController = require('fs');
+const fs = require('fs');
+const formate = require('dateformat');
+const padding = require('pad');
+const sync = require('async');
 
-router.get("/",function(req,resp)
-{
-  resp.render("frontpage");
+router.get("/", function(req, resp) {
+    resp.render("frontpage");
 });
 
-router.post("/",function(req,resp)
-{
-UserPayment.find({} ,function (error , usersdata)
-{
-usersdata.each(function(err,data)
-{
-  console.log(data.Account);
-});
+router.post("/", function(req, resp) {
+    const header = "E" + padding(req.body.id, 13) + padding(req.body.name, 10) + padding(req.body.account, 15) + formate(new Date(), 'yyyymmdd');
+    var count = 0;
+    var sum = 0.0;
+    sync.series([
+        function(callback) {
+            fs.appendFile('unapec-appap.txt', header + "\n", callback);
+        },
+
+        function(callback) {
+            UserPayment.find({}, function(error, usersdata) {
+                count = usersdata.length;
+                usersdata.forEach(function(user) {
+                    sum += user.Amount;
+                    fs.appendFile('unapec-appap.txt', 'D' + padding(user.Id, 13) + user.IdType + padding(user.Account, 15) + padding(user.Amount, 20), function(err) {
+                        console.log(err);
+                    });
+                });
+                callback();
+            });
+        },
+        function(callback) {
+            fs.appendFile('unapec-appap.txt', 'S' + padding(count + "", 9, "") + padding(sum + ".", 20, '0') + "\n");
+            callback();
+        }
+    ], function(error, results) {});
+
+    resp.render("frontpage", {
+        message: "Se ha generado el archivo con exito"
+    });
+
 
 });
 
 
-
-  resp.render("frontpage",{message:"Se ha generado el archivo con exito"});
-});
-
-
-module.exports=router;
+module.exports = router;
